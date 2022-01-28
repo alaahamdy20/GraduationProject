d67b75134bbffd4cc4a1f526bdfdfa1022bcf549
alaahamdy20 <alaahamdy201295@gmail.com>
2022-01-28 20:10:59 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { HeaderinfoComponent } from './Components/headerinfo/headerinfo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderinfoComponent
   ],
   imports: [
     BrowserModule,
