3a82d4bf482a40e8d646e78bb4115b75ac3e2f6a
MahmoudSabry70 <mahmoud.sabry.khuzaim@gmail.com>
2022-01-30 22:07:49 +0200

    adding slider component

---
+++ src/app/Components/slide-photo/slide-photo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-photo',
+  templateUrl: './slide-photo.component.html',
+  styleUrls: ['./slide-photo.component.scss']
+})
+export class SlidePhotoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
